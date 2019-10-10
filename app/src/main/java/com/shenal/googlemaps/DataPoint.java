767b358d16e8ce235345afd6aabf2e20210812a9
cavindi <cavindi.j7@gmail.com>
2019-10-10 15:50:08 +0100

    adding firebase

---
+++ app/src/main/java/com/shenal/googlemaps/DataPoint.java
@@ -0,0 +1,21 @@
+package com.shenal.googlemaps;
+
+public class DataPoint {
+    int xValue, yValue;
+
+    public DataPoint(int xValue, int yValue) {
+        this.xValue = xValue;
+        this.yValue = yValue;
+    }
+
+    public DataPoint() {
+    }
+
+    public int getxValue() {
+        return xValue;
+    }
+
+    public int getyValue() {
+        return yValue;
+    }
+}
---
+++ app/src/main/java/com/shenal/googlemaps/FirebaseActivity.java
@@ -0,0 +1,102 @@
+package com.shenal.googlemaps;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class FirebaseActivity extends AppCompatActivity {
+
+    EditText xValue, yValue;
+    Button btnSave;
+    LineChart lineChart;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    LineDataSet lineDataSet = new LineDataSet(null, null);
+    ArrayList<ILineDataSet> iLineDataSets = new ArrayList<>();
+    LineData lineData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firebase);
+
+        xValue = findViewById(R.id.xValue);
+        yValue = findViewById(R.id.yValue);
+        btnSave = findViewById(R.id.btnSave);
+        lineChart = findViewById(R.id.line_chart);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("ChartValues");
+        insertData();
+    }
+
+    private void insertData() {
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String id = databaseReference.push().getKey();
+                int x = Integer.parseInt(xValue.getText().toString());
+                int y = Integer.parseInt(yValue.getText().toString());
+
+                DataPoint dataPoint = new DataPoint(x, y);
+                databaseReference.child(id).setValue(dataPoint);
+
+                retrieveData();
+
+            }
+        });
+    }
+
+    private void retrieveData() {
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                ArrayList<Entry> dataVals = new ArrayList<Entry>();
+
+                if (dataSnapshot.hasChildren()) {
+                    for (DataSnapshot myDataSnapshot : dataSnapshot.getChildren()) {
+                        DataPoint dataPoint = myDataSnapshot.getValue(DataPoint.class);
+                        dataVals.add(new Entry(dataPoint.getxValue(), dataPoint.getyValue()));
+                    }
+                    showChart(dataVals);
+                } else {
+                    lineChart.clear();
+                    lineChart.invalidate();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void showChart(ArrayList<Entry> dataVals) {
+        lineDataSet.setValues(dataVals);
+        lineDataSet.setLabel("Data Set 1");
+        iLineDataSets.clear();
+        iLineDataSets.add(lineDataSet);
+        lineData = new LineData(iLineDataSets);
+        lineChart.clear();
+        lineChart.setData(lineData);
+        lineChart.invalidate();
+        
+    }
+}
