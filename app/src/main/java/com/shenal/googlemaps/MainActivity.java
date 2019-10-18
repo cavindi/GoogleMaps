fe60519ba3555aa6d1f547fca91a157c881de838
cavindi <cavindi.j7@gmail.com>
2019-10-18 20:43:51 +0100

    adding image buttons

--- app/src/main/java/com/shenal/googlemaps/MainActivity.java
+++ app/src/main/java/com/shenal/googlemaps/MainActivity.java
@@ -1,14 +1,49 @@
 package com.shenal.googlemaps;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton mapBtn, lineChartBtn;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main3);
+
+        mapBtn = findViewById(R.id.mapButton);
+        lineChartBtn = findViewById(R.id.lineChartButton);
+
+        mapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToMap();
+
+            }
+        });
+
+        lineChartBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToLineChart();
+
+            }
+        });
     }
+
+    public void goToMap(){
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToLineChart(){
+        Intent intent = new Intent(this, FirebaseActivity.class);
+        startActivity(intent);
+    }
+
 }
